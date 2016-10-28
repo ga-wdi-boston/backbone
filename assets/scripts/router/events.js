482560cdd6c3e57cfeda18332c36bb616290918c
berziiii <brian@offpistedesigns.com>
2016-10-28 12:40:39 -0400

    functional routes with all content rendered

--- assets/scripts/router/events.js
+++ assets/scripts/router/events.js
@@ -1,11 +1,18 @@
 'use strict';
 
+const router = require('./index');
 const ui = require('./ui');
 
+const routes = router.rootNode.children.map((route) => {
+  return route.name;
+});
+
 const registerPaths = () => {
-  require('./paths').forEach((path) => {
-    $(`a[href="${path}"]`).on('click', (event) => {
+  routes.forEach((route) => {
+    $(`a[href="#${route}"]`).on('click', (event) => {
       event.preventDefault();
+
+      // router.navigate(route);
 
       ui.hideAllContent();
       ui.showContent(path);
--- assets/scripts/router/index.js
+++ assets/scripts/router/index.js
@@ -1,7 +1,28 @@
 'use strict';
 
+const Router5 = require('router5').default;
+const loggerPlugin = require('router5').loggerPlugin();
+
+const routes = [
+      { name: 'index', path: '/'},
+      { name: 'about', path: '/about' },
+      { name: 'projects', path: '/projects' },
+      { name: 'talks', path: '/talks' },
+];
+
+const options = {
+    useHash: true,
+    hashPrefix: '!',
+    default: '/',
+};
+
+const router = new Router5(routes, options)
+.usePlugin(loggerPlugin);
+
 $(() => {
   require('./events').registerPaths();
 });
 
-module.exports = true;
+module.exports = {
+  router,
+};
